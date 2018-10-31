e80c3a5c0d545c2092eb7abc2e270db03cedc54a
Alisa McCormick <alisa.cookie.mccormick@gmail.com>
2018-10-31 18:02:47 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,9 +6,10 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if !BRANDS.include?(brand)
-      BRANDS << brand
-    end
+    BRANDS.uniq
+    # if !BRANDS.include?(brand)
+    #   BRANDS << brand
+    # end
   end
 
   def cobble
